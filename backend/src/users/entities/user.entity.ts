c792c072d42eeeb6ab3655c8763c0e5af936bfff
Shawn GB <god.shawn@gmail.com>
2026-02-03 14:27:20 +0100

    feat(users): add fields and TypeORM decorators to User entity
    This commit adds the following fields to the User entity:
    - id (PrimaryGeneratedColumn)
    - role (Column with UserRole enum)
    - email (unique Column)
    - firstName (Column)
    - lastName (Column)
    
    It also imports UserRole from src/types/enums and TypeORM decorators.

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { UserRole } from 'src/types/enums';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.GUEST })
+  role: UserRole;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ name: 'first_name', length: 150 })
+  firstName: string;
+
+  @Column({ name: 'last_name', length: 150 })
+  lastName: string;
+}
